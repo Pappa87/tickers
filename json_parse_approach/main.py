8597650358ab3f5957d6b0455ddbdd899249b2ef
Papp Albert <albert.papp@utopusinsights.com>
2025-03-07 16:59:27 +0100

    scheduler done

--- json_parse_approach/main.py
+++ json_parse_approach/main.py
@@ -48,10 +48,11 @@ def try_to_run_parser(link, today_string):
 def job_to_run():
     download("wallstreetbets")
 
-
+print("reddit scraper started: \n")
+# make it run without scheduling first:)
+job_to_run()
 # Schedule the task
-schedule.every(5).minutes.do(job_to_run)
-
+schedule.every(1).minutes.do(job_to_run)
 while True:
     schedule.run_pending()
     time.sleep(1)
