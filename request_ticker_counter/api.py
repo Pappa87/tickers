5dd2fee838522c61fe41ddaccab6de57584ff656
Papp Albert <albertxpapp@gmail.com>
2025-03-09 09:56:41 +0100

    server config updated

--- request_ticker_counter/api.py
+++ request_ticker_counter/api.py
@@ -1,10 +1,13 @@
+from gevent.pywsgi import WSGIServer
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def example():
-   return '{"name":"Bob"}'
+def index():
+    return "Hello, HTTP!"
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Host and port for the HTTP server
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
